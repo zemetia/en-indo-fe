ff1660f75921ba828e7ac1c01856f448231a3507
ZYMER Corp <SClous2012@gmail.com>
2025-07-04 11:15:00 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/dashboard/jemaat/page.tsx
+++ src/app/dashboard/jemaat/page.tsx
@@ -27,6 +27,7 @@ import {
   AlertDialogCancel,
   AlertDialogContent,
   AlertDialogDescription,
+  AlertDialogFooter,
   AlertDialogHeader,
   AlertDialogTitle,
 } from '@/components/ui/alert-dialog';
