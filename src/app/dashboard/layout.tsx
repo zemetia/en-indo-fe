b3617e61477544fac64bfa88e1fba4c6d50fcafb
ZYMER Corp <SClous2012@gmail.com>
2025-07-04 07:42:21 +0000

    still i can not access the dashboard, please check the guard or if you c

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -3,7 +3,6 @@
 import * as React from 'react';
 
 import Sidebar from '@/components/navigation/Sidebar';
-import PermissionGuard from '@/components/auth/PermissionGuard';
 
 export default function DashboardLayout({
   children,
@@ -11,11 +10,9 @@ export default function DashboardLayout({
   children: React.ReactNode;
 }) {
   return (
-    <PermissionGuard>
-      <div className='flex min-h-screen bg-gray-50'>
-        <Sidebar />
-        <main className='flex-1 p-8'>{children}</main>
-      </div>
-    </PermissionGuard>
+    <div className='flex min-h-screen bg-gray-50'>
+      <Sidebar />
+      <main className='flex-1 p-8'>{children}</main>
+    </div>
   );
 }
